fcca50abce23456d80c8e81284778a4ba7129c1b
petrovicaca <acapetrovic24@gmail.com>
2021-01-09 16:05:57 +0100

    findOne

--- NodeJS/controllers/profileController.js
+++ NodeJS/controllers/profileController.js
@@ -1,5 +1,6 @@
 const express = require('../../ispit/node_modules/express');
 const db = require('../db');
+const _ = require('../../ispit/node_modules/lodash');
 
 var router = express.Router();
 var ObjectId = require('../../ispit/node_modules/mongoose').Types.ObjectId;
@@ -15,6 +16,24 @@ router.get('/', (req, res) => {
         else { console.log('Error in Retriving Profiles:' + JSON.stringify(err, undefined, 2)); }
     });
 });
+
+
+router.get('/:username/:password', (req, res) => {
+    Profile.findOne({ username: req.params.username, password: req.params.password}, (err, docs) => {
+        if (!err) { res.send(docs); }
+        else { console.log('Error in Retriving Profiles:' + JSON.stringify(err, undefined, 2)); }    
+    });
+});
+    
+    /*
+    Profile.findOne({ username: req.username}).exec(function (err, profile){
+        if (err) return handleError(err);
+        console.log('The author is %s', profile.username);
+    });
+    
+    */
+
+
 
 /*
 router.get('/:username', (req, res) => {
