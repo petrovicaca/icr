ed9186521b9ec0edce6bbb090874ea323ef5e3dc
MarkoCupic98 <cupic.marko998@gmail.com>
2021-01-13 12:49:04 +0100

    back button saving stuff xD

--- ispit/src/app/profile/profile.component.ts
+++ ispit/src/app/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { LoginService } from '../shared/login.service';
 import { Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NgForm } from '@angular/forms';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -15,7 +16,7 @@ export class ProfileComponent implements OnInit{
   isEditing: boolean = false;
   profileForInput: Profile;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public loginService: LoginService) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public loginService: LoginService, private location: Location) { }
 
   ngOnInit(): void {
     this.profileForInput = {
@@ -35,6 +36,10 @@ export class ProfileComponent implements OnInit{
     this.data.user.address = this.profileForInput.address;
     console.log(this.data.user);
     console.log(LoginService.dummyUserList);
+    this.isEditing = false;
+  }
+
+  goBack() {
     this.isEditing = false;
   }
   
